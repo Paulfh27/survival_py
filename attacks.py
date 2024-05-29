086f4e8a0c5c9504cba5ba61c42617c385601009
Paulfh27 <paulfhebertjr@gmail.com>
2024-05-28 20:37:56 -0400

    set a slash png for the sword that orients itself based on direction.

--- attacks.py
+++ attacks.py
@@ -3,7 +3,15 @@ from sprites import *
 
 RED = (255,0,0)
 RELOAD = -60        # reload speed, the lower the number the longer the reload
-SLASH = ""
+SLASH = "./assets/images/slash.png"
+
+slash = pygame.image.load(SLASH)
+slash = pygame.transform.scale(slash, (40,40))
+
+slash_up = pygame.transform.rotate(slash, 90)
+slash_right = slash
+slash_down = pygame.transform.rotate(slash, 270)
+slash_left = pygame.transform.flip(slash, True, False)
 
 class Sword(pygame.sprite.Sprite): 
     def __init(self):
@@ -12,7 +20,9 @@ class Sword(pygame.sprite.Sprite):
     def addHolder(self, holder): 
         self.holder = holder
         holder.attack = self
-        self.image = pygame.Surface((self.holder.rect.width, self.holder.rect.height))
+        #self.image = pygame.Surface((self.holder.rect.width, self.holder.rect.height))
+        self.image = pygame.image.load(SLASH)
+        self.image = pygame.transform.scale(self.image, (40, 40))
         self.rect = self.image.get_rect()
         self.image.fill((0,0,0,0)) 
         self.active = False
@@ -24,7 +34,6 @@ class Sword(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and not self.active and self.time <= RELOAD: 
             self.time = 10
-            self.image.fill((RED))
             self.image.set_alpha((255))
             self.active = True
         if self.time <= 0: 
@@ -36,11 +45,15 @@ class Sword(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_DOWN]: 
             self.rect.topleft = self.holder.rect.bottomleft
+            self.image = slash_down
         elif keys[pygame.K_LEFT]: 
             self.rect.bottomright = self.holder.rect.bottomleft
+            self.image = slash_left
         elif keys[pygame.K_RIGHT]: 
             self.rect.bottomleft = self.holder.rect.bottomright
+            self.image = slash_right
         elif keys[pygame.K_UP]: 
             self.rect.bottomleft = self.holder.rect.topleft
+            self.image = slash_up
         self.attack()
         
